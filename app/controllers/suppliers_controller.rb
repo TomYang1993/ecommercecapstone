8fb0bba6961599bd5bc8c436ca01825ba701f583
Tom Yang <xy2286@columbia.edu>
2016-08-06 21:19:35 -0400

    add supplier

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,2 +1,24 @@
 class SuppliersController < ApplicationController
+  def index
+    @supplier = Supplier.all
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    supplier = Supplier.new(
+      name: params[:name],
+      email: params[:email],
+      phone: params[:phone]
+    )
+    supplier.save
+    redirect_to "/suppliers/#{supplier.id}"
+  end
+
+  def show
+    @supplier = Supplier.find_by(id: params[:id])
+  end
+
 end
